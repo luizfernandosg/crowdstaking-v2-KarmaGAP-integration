9259ddd94f1eeef49fa73b078cca056b75ed4a41
subject026 <subject026@protonmail.com>
2023-10-25 17:53:31 +0100

    fix lint error

--- src/app/bakery/hooks/useReadings.tsx
+++ src/app/bakery/hooks/useReadings.tsx
@@ -1,21 +1,20 @@
-import { TUserConnected } from "@/app/core/hooks/useConnectedUser";
+// import { TUserConnected } from "@/app/core/hooks/useConnectedUser";
 
-function useReadings({user}: {user: TUserConnected}) {
+// function useReadings({user}: {user: TUserConnected}) {
 
-    const { DAI, BREAD } = user.config;
+//     const { DAI, BREAD } = user.config;
 
-  const breadBalanceReadings = useTokenBalance(BREAD.address, user.address);
-  const daiBalanceReadings = useTokenBalance(DAI.address, user.address);
+//   const breadBalanceReadings = useTokenBalance(BREAD.address, user.address);
+//   const daiBalanceReadings = useTokenBalance(DAI.address, user.address);
 
-  const {
-    value: daiAllowanceValue,
-    status: daiAllowanceStatus,
-    error: daiAllowanceError,
-  } = useTokenAllowance(DAI.address, user.address, BREAD.address);
+//   const {
+//     value: daiAllowanceValue,
+//     status: daiAllowanceStatus,
+//     error: daiAllowanceError,
+//   } = useTokenAllowance(DAI.address, user.address, BREAD.address);
 
-}
+// }
 
-function WithReadings
+// // function WithReadings
 
-
-export {useReadings}
+// export {useReadings}
