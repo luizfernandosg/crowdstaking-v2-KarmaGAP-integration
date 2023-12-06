43513b80a38f4208d10f2025d5a09c2e51dcc4a5
subject026 <subject026@protonmail.com>
2023-12-06 10:39:24 +0000

    bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  minify: false,
   output: "export",
   images: {
     unoptimized: true,
   },
 };
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: "true",
+});
+
+// module.exports = withBundleAnalyzer(nextConfig);
 module.exports = nextConfig;
