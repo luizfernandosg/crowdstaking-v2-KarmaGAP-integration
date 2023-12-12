e907608c46ae8528941145906f5d7875d08bf6a8
subject026 <subject026@protonmail.com>
2023-12-12 16:29:59 +0000

    hook up burn logic

--- src/app/bakery/components/Swap/Burn.tsx
+++ src/app/bakery/components/Swap/Burn.tsx
@@ -1,8 +1,23 @@
-import { usePrepareContractWrite } from "wagmi";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
+import { parseEther } from "viem";
+import {
+  Root as DialogPrimitiveRoot,
+  Portal as DialogPrimitivePortal,
+  Overlay as DialogPrimitiveOverlay,
+  Trigger as DialogPrimitiveTrigger,
+  Content as DialogPrimitiveContent,
+  Close as DialogPrimitiveClose,
+} from "@radix-ui/react-dialog";
 
 import { TUserConnected } from "@/app/core/hooks/useConnectedUser";
+import { BREAD_GNOSIS_ABI } from "@/abi";
 import Button from "@/app/core/components/Button";
 import config from "@/config";
+import useDebounce from "@/app/bakery/hooks/useDebounce";
 
 export default function Burn({
   user,
@@ -11,12 +26,54 @@ export default function Burn({
   user: TUserConnected;
   inputValue: string;
 }) {
-  config[user.chain.id];
-  // const {} = usePrepareContractWrite();
+  const { BREAD } = config[user.chain.id];
+
+  const debouncedValue = useDebounce(inputValue, 500);
+
+  const parsedValue = parseEther(
+    debouncedValue === "." ? "0" : debouncedValue || "0"
+  );
+
+  const {
+    config: prepareConfig,
+    status: prepareStatus,
+    error: prepareError,
+  } = usePrepareContractWrite({
+    address: BREAD.address,
+    abi: BREAD_GNOSIS_ABI,
+    functionName: "burn",
+    args: [parsedValue, user.address],
+    enabled: parseFloat(debouncedValue) > 0,
+  });
+
+  console.log({ prepareError });
+
+  const {
+    write,
+    isLoading: writeIsLoading,
+    isError: writeIsError,
+    error: writeError,
+    isSuccess: writeIsSuccess,
+    data: writeData,
+  } = useContractWrite(prepareConfig);
+
+  const { data } = useContractRead({
+    address: BREAD.address,
+    abi: BREAD_GNOSIS_ABI,
+    functionName: "maxWithdraw",
+  });
+
+  console.log({ maxWithdraw: data });
 
   return (
     <div className="p-2 w-full flex flex-col gap-2">
-      <Button fullWidth={true} variant="large" onClick={() => {}}>
+      <Button
+        fullWidth={true}
+        variant="large"
+        onClick={() => {
+          write?.();
+        }}
+      >
         Burn
       </Button>
     </div>
