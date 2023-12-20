0ff12cc4f88869cb1c38568edb9d34422e3dff4b
subject026 <subject026@protonmail.com>
2023-12-20 16:29:35 +0000

    dynamic imports

--- src/app/core/hooks/WagmiProvider/config/getConfig.ts
+++ src/app/core/hooks/WagmiProvider/config/getConfig.ts
@@ -1,13 +1,12 @@
-import { devConfig, devChains } from "./devConfig";
-import { prodConfig, prodChains } from "./prodConfig";
-
 export function getConfig() {
   switch (process.env.NODE_ENV) {
     case "development":
+      const { devConfig, devChains } = require("./devConfig");
       return { chains: devChains, config: devConfig };
     case "production":
+      const { prodConfig, prodChains } = require("./prodConfig");
       return { chains: prodChains, config: prodConfig };
     default:
-      throw new Error("unable to get config!!");
+      throw new Error("unable to get wagmi config!!");
   }
 }
