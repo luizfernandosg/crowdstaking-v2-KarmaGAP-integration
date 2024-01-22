4287892cb7825c5d5502fc7ca5a7d311c878b56f
subject026 <subject026@protonmail.com>
2024-01-22 09:06:16 +0000

    test config

---
+++ src/app/core/hooks/WagmiProvider/config/testConfig.ts
@@ -0,0 +1,66 @@
+import { configureChains, createConfig } from "wagmi";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+import { hardhat } from "wagmi/chains";
+import {
+  connectorsForWallets,
+  getDefaultWallets,
+} from "@rainbow-me/rainbowkit";
+import {
+  argentWallet,
+  injectedWallet,
+  ledgerWallet,
+  trustWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { publicProvider } from "wagmi/providers/public";
+
+const WALLET_CONNECT_PROJECT_ID =
+  process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID;
+if (!WALLET_CONNECT_PROJECT_ID)
+  throw new Error("WALLET_CONNECT_PROJECT_ID not set!");
+
+const chainsConfig = configureChains(
+  [{ ...hardhat, id: 31337 }],
+  [
+    publicProvider(),
+    jsonRpcProvider({
+      rpc: () => ({
+        http: "http://localhost:8545",
+        webSocket: "ws://localhost:8545",
+      }),
+    }),
+  ]
+);
+
+const { chains } = chainsConfig;
+
+const { publicClient } = chainsConfig;
+
+const projectId = WALLET_CONNECT_PROJECT_ID;
+
+const { wallets } = getDefaultWallets({
+  appName: "Breadchain Crowdstaking",
+  projectId,
+  chains,
+});
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: "Other",
+    wallets: [
+      injectedWallet({ chains }),
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+      ledgerWallet({ projectId, chains }),
+    ],
+  },
+]);
+
+const config = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+export { chains, config };
--- src/chainConfig.ts
+++ src/chainConfig.ts
@@ -15,41 +15,61 @@ export interface IConfig {
   [chainId: number]: ChainConfiguration;
 }
 
-const config: IConfig =
-  process.env.NODE_ENV === "development"
-    ? {
-        31337: {
-          ID: 31337,
-          NETWORK_STRING: "Hardhat",
-          EXPLORER: "NONE",
-          BREAD: {
-            symbol: "BREAD",
-            decimals: 18,
-            address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
-          },
+function getConfig(): IConfig {
+  if (process.env.NODE_ENV === "development") {
+    return {
+      31337: {
+        ID: 31337,
+        NETWORK_STRING: "Hardhat",
+        EXPLORER: "NONE",
+        BREAD: {
+          symbol: "BREAD",
+          decimals: 18,
+          address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
         },
-        100: {
-          ID: 100,
-          NETWORK_STRING: "Gnosis",
-          EXPLORER: "https://gnosisscan.io",
-          BREAD: {
-            symbol: "BREAD",
-            decimals: 18,
-            address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
-          },
+      },
+      100: {
+        ID: 100,
+        NETWORK_STRING: "Gnosis",
+        EXPLORER: "https://gnosisscan.io",
+        BREAD: {
+          symbol: "BREAD",
+          decimals: 18,
+          address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
         },
-      }
-    : {
-        100: {
-          ID: 100,
-          NETWORK_STRING: "Gnosis",
-          EXPLORER: "https://gnosisscan.io",
-          BREAD: {
-            symbol: "BREAD",
-            decimals: 18,
-            address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
-          },
+      },
+    };
+  }
+
+  if (process.env.NEXT_PUBLIC_TEST_CONNECTOR === "true") {
+    return {
+      31337: {
+        ID: 31337,
+        NETWORK_STRING: "Hardhat",
+        EXPLORER: "NONE",
+        BREAD: {
+          symbol: "BREAD",
+          decimals: 18,
+          address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
         },
-      };
+      },
+    };
+  }
+
+  return {
+    100: {
+      ID: 100,
+      NETWORK_STRING: "Gnosis",
+      EXPLORER: "https://gnosisscan.io",
+      BREAD: {
+        symbol: "BREAD",
+        decimals: 18,
+        address: "0x2993bD6dA994378Eadf1A5ba5c9017FdFc92f111",
+      },
+    },
+  };
+}
+
+const config: IConfig = getConfig();
 
 export default config;
