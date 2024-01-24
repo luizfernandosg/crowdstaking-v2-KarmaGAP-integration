f7085edcdfca7508ebbbd8e5ec1a7ba4c23dd732
subject026 <subject026@protonmail.com>
2024-01-24 20:46:51 +0000

    fix script

--- dev.sh
+++ dev.sh
@@ -4,10 +4,10 @@ tmux new-session -d -s breadchain
 
 tmux new-window -t breadchain:1 -n chain 
 tmux rename-window -t breadchain:1 "chain"
-tmux send-keys -t breadchain:1 "pnpm hardhat:chain" Enter
+tmux send-keys -t breadchain:1 "pnpm hardhat:node" Enter
 tmux split-window -dt breadchain:1
 tmux split-window -ht breadchain:1
-tmux send-keys -t breadchain:1 "pnpm next:dev" Enter
+tmux send-keys -t breadchain:1 "pnpm run dev" Enter
 
 tmux select-window -t breadchain:1
 tmux attach-session -d -t breadchain
