99aa25c6f0be10260d97336faec194c52128a689
subject026 <subject026@protonmail.com>
2024-02-10 11:22:02 +0000

    add button styles

--- src/app/bakery/components/Swap/InsufficentBalance.tsx
+++ src/app/bakery/components/Swap/InsufficentBalance.tsx
@@ -1,15 +1,17 @@
 import { ButtonVariants } from "@/app/core/components/Button/Button";
 import clsx from "clsx";
+import Image from "next/image";
 
 export function InsufficentBalance() {
   return (
     <div
       className={clsx(
         ButtonVariants["large"],
-        "w-full bg-breadgray-og-dark text-breadgray-grey rounded-xl font-medium text-center"
+        "flex justify-center gap-4 w-full bg-breadgray-og-dark text-status-danger rounded-xl font-medium text-center border-2 border-status-danger"
       )}
     >
-      Insufficient balance
+      <Image src="warning_icon.svg" width={25} height={24} alt="warning icon" />
+      Insufficient Funds
     </div>
   );
 }
