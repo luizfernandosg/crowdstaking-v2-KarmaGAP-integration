c9c76c355ea0270ae5996c3255754606dd5fa328
subject026 <subject026@protonmail.com>
2024-03-16 14:09:05 +0000

    new hardhat rpc

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox-viem");
 require("dotenv").config();
 
-const QUIKNODE_URL = process.env.QUIKNODE_URL;
+const RPC_URL = process.env.RPC_URL;
 const FORK_BLOCK_NUMBER = process.env.FORK_BLOCK_NUMBER
   ? parseInt(process.env.FORK_BLOCK_NUMBER)
   : undefined;
 
-if (!QUIKNODE_URL || !FORK_BLOCK_NUMBER) throw new Error("check env vars!");
+if (!RPC_URL || !FORK_BLOCK_NUMBER) throw new Error("check env vars!");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -16,14 +16,13 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: QUIKNODE_URL,
-        blockNumber: FORK_BLOCK_NUMBER,
+        url: RPC_URL,
+        // blockNumber: FORK_BLOCK_NUMBER,
       },
       mining: {
         auto: false,
         interval: 3000,
       },
-      hardfork: "merge",
     },
   },
 };
