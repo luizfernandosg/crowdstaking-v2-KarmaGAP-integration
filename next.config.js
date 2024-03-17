a1957eb0378726f85bb08792214c12340e57fa51
subject026 <subject026@protonmail.com>
2024-03-17 13:05:14 +0000

    headers for manifest

--- next.config.js
+++ next.config.js
@@ -7,16 +7,21 @@ const nextConfig = {
   async headers() {
     return [
       {
-        key: "Access-Control-Allow-Origin",
-        value: "*",
-      },
-      {
-        key: "Access-Control-Allow-Methods",
-        value: "GET",
-      },
-      {
-        key: "Access-Control-Allow-Headers",
-        value: "X-Requested-With, content-type, Authorization",
+        source: "/manifest.json",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, content-type, Authorization",
+          },
+        ],
       },
     ];
   },
