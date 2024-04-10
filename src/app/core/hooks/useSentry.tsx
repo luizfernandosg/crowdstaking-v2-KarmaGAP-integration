7bdcf66e14af024896f96551e8ea55813a532c0c
subject026 <subject026@protonmail.com>
2024-04-10 11:15:44 +0100

    fix sentry config

--- src/app/core/hooks/useSentry.tsx
+++ src/app/core/hooks/useSentry.tsx
@@ -1,9 +1,8 @@
 import * as Sentry from "@sentry/react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 export function useSentry() {
   useEffect(() => {
-    // if (!isSentryReady) {
     if (process.env.NEXT_PUBLIC_SENTRY_DSN)
       Sentry.init({
         dsn: "https://88428fa48e306d2d0b486226b7281b74@o4505884558360576.ingest.us.sentry.io/4506695825162240",
@@ -11,17 +10,12 @@ export function useSentry() {
         tracesSampleRate: 1.0,
 
         // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: [
-          "localhost",
-          /^https:\/\/yourserver\.io\/api/,
-        ],
+        tracePropagationTargets: ["localhost"],
 
         // Capture Replay for 10% of all sessions,
         // plus for 100% of sessions with an error
         replaysSessionSampleRate: 0.1,
         replaysOnErrorSampleRate: 1.0,
       });
-    // }
-    // setIsSentryReady(true);
   }, []);
 }
