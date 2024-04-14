07afbcb7caf032d1a95d8430c86bb15be45fa30e
subject026 <subject026@protonmail.com>
2024-04-14 11:35:49 +0100

    add green dot

--- src/app/bakery/components/TotalSupply.tsx
+++ src/app/bakery/components/TotalSupply.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ERC20_ABI } from "@/abi";
-import Elipsis from "@/app/core/components/Elipsis";
 import { formatSupply } from "@/app/core/util/formatter";
 import { ReactNode } from "react";
 import { formatUnits } from "viem";
@@ -15,17 +14,14 @@ export function TotalSupply() {
     watch: true,
   });
 
-  console.log({ data });
-
   return (
     <div className="flex justify-center">
-      <div className="flex gap-2 p-2 bg-breadgray-charcoal rounded">
+      <div className="flex items-center gap-2 p-2 bg-breadgray-charcoal rounded">
+        <span className="rounded-full w-2 h-2 bg-status-success" />
         <span className="text-xl font-semibold">
-          {data ? (
-            formatSupply(parseInt(formatUnits(BigInt(data as string), 18)))
-          ) : (
-            <Elipsis />
-          )}
+          {data
+            ? formatSupply(parseInt(formatUnits(BigInt(data as string), 18)))
+            : "--.--"}
         </span>
         <GradientText>$BREAD</GradientText>
         <span className="font-bold text-xl">strong</span>
