1e8de5e460910628ffe9623308fcbd57b987c8d3
subject026 <subject026@protonmail.com>
2024-05-03 14:50:02 +0100

    fix svg attributes

--- src/app/core/components/Icons/Logo.tsx
+++ src/app/core/components/Icons/Logo.tsx
@@ -2,8 +2,8 @@ export function LogoSVG() {
   return (
     <svg viewBox="0 0 60 58" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M10.4348 0.5C4.67181 0.5 0 5.17181 0 10.9348C0 16.3448 4.11705 20.7932 9.38895 21.3178V44.8476C9.38895 52.0514 15.2287 57.8911 22.4324 57.8911H37.0408C44.2445 57.8911 50.0842 52.0514 50.0842 44.8477V21.3569C55.6061 21.0864 60 16.5237 60 10.9348C60 5.17181 55.3282 0.5 49.5652 0.5L10.4348 0.5Z"
         fill="url(#paint0_linear_2974_31682)"
       />
@@ -16,8 +16,8 @@ export function LogoSVG() {
           y2="9.45708"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#D04EC5" />
-          <stop offset="1" stop-color="#ED7BC7" />
+          <stop stopColor="#D04EC5" />
+          <stop offset="1" stopColor="#ED7BC7" />
         </linearGradient>
       </defs>
     </svg>
