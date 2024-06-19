78c9cee497e15514126a933ba3b3affe7c559009
subject026 <subject026@protonmail.com>
2024-06-19 11:14:53 +0100

    correct colors for light mode

--- src/app/bakery/components/Swap/InsufficentBalance.tsx
+++ src/app/bakery/components/Swap/InsufficentBalance.tsx
@@ -7,10 +7,23 @@ export function InsufficentBalance() {
     <div
       className={clsx(
         ButtonVariants["large"],
-        "flex justify-center gap-4 w-full dark:bg-breadgray-og-dark text-status-danger rounded-xl font-medium text-center border-2 border-status-danger"
+        "flex justify-center items-center gap-4 w-full dark:bg-breadgray-og-dark text-status-danger-light dark:text-status-danger rounded-xl font-medium text-center border-2 border-status-danger-light dark:border-status-danger"
       )}
     >
-      <Image src="warning_icon.svg" width={25} height={24} alt="warning icon" />
+      <svg
+        className="fill-current"
+        width="25"
+        height="24"
+        viewBox="0 0 25 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M3.5 3H5.49976H5.5H19.4998V5H5.5V19H19.4998V21L5.5 21L5.49976 21L3.5 21V3ZM21.5 3H19.5V21H21.5V3ZM11.5 15.0001H13.5V17.0001H11.5V15.0001ZM13.5 7H11.5V13H13.5V7Z"
+        />
+      </svg>
       Insufficient Funds
     </div>
   );
