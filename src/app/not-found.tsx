dfb0c4d31e26a9ee3271a59101e6ab4dcf3557aa
Sara <sara@Saras-MBP.home>
2024-10-14 08:58:36 -0700

    Update 404 page
    [#57]

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,26 @@
-import Link from "next/link";
+import ButtonLink from "@/app/core/components/ButtonLink/ButtonLink";
 
 export default function NotFound() {
   return (
     <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+      <div className="flex flex-col items-center my-10">
+        <h1 className="text-[2.5rem] font-pressstart uppercase leading-tight">
+          <span>404</span>
+        </h1>
+        <h2 className="uppercase md:text-2xl font-normal">
+          <span>Page not found</span>
+        </h2>
+        <hr className="w-1/4 max-w-[160px] border-breadpink-shaded my-6" />
+        <div className="flex flex-col items-center max-w-[250px]">
+          <p className="leading-tight text-center">
+            Post-Capitalism can only be achieved through perserverance.
+          </p>
+          <p className="leading-tight text-center">Please try again later.</p>
+        </div>
+        <ButtonLink href="/" additionalClasses="my-6">
+          Return home
+        </ButtonLink>
+      </div>
     </div>
   );
 }
