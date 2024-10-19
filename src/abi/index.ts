7ee95755b2bff7cfebdd4a9b0de87cd9ed4964d5
subject026 <subject026@protonmail.com>
2024-10-19 12:20:30 +0100

    update distributor ABI

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -1,8 +1,21 @@
-import BREAD from "./Bread.json";
-import DISTRIBUTOR from "./Distributor.json";
-import ERC20 from "./ERC20.json";
 import SDAI_ADAPTOR from "./SDAIAdaptor.json";
-import BUTTERED_BREAD from "./ButteredBread.json";
+import ERC20 from "./ERC20.json";
+
+import BREAD_PROD from "./Bread.json";
+import DISTRIBUTOR_PROD from "./Distributor.json";
+import BUTTERED_BREAD_PROD from "./ButteredBread.json";
+
+let BREAD = BREAD_PROD;
+let DISTRIBUTOR = DISTRIBUTOR_PROD;
+let BUTTERED_BREAD = BUTTERED_BREAD_PROD;
+
+if (process.env.NODE_ENV === "development") {
+  BREAD = require("../../contracts/out/Bread.sol/Bread.json").abi;
+  DISTRIBUTOR =
+    require("../../contracts/out/YieldDistributor.sol/YieldDistributor.json").abi;
+  BUTTERED_BREAD =
+    require("../../contracts/out/ButteredBread.sol/ButteredBread.json").abi;
+}
 
 export const BREAD_ABI = BREAD;
 export const ERC20_ABI = ERC20;
