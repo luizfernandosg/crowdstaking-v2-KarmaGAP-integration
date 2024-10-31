77c38ac8c172056b1bf455f4deb4f93faf3fd317
subject026 <subject026@protonmail.com>
2024-10-31 16:37:51 +0000

    fix button text size

--- src/app/core/components/MaxButton.tsx
+++ src/app/core/components/MaxButton.tsx
@@ -10,7 +10,7 @@ export function MaxButton({
   return (
     <button
       type="button"
-      className="px-2 font-bold text-breadviolet-violet dark:text-breadpink-pink text-sm"
+      className="px-2 font-bold text-breadviolet-violet dark:text-breadpink-pink text-xs"
       onClick={onClick}
     >
       {children}
