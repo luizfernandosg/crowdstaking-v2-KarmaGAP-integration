8946c80a7f0a545280f99d68ea486e95f3c83da2
subject026 <subject026@protonmail.com>
2024-10-31 14:44:45 +0000

    make "use max" button reusable

---
+++ src/app/core/components/MaxButton.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+export function MaxButton({
+  children,
+  onClick,
+}: {
+  children: ReactNode;
+  onClick: () => void;
+}) {
+  return (
+    <button
+      type="button"
+      className="px-2 font-bold text-breadviolet-violet dark:text-breadpink-shaded text-sm"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
