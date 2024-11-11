f3570aede9c5eeeb54eb98662e9e210c9b3037da
subject026 <subject026@protonmail.com>
2024-11-11 21:56:05 +0000

    fix svg attributes

--- src/app/core/components/Icons/Bread.tsx
+++ src/app/core/components/Icons/Bread.tsx
@@ -9,8 +9,8 @@ export function BreadSVG({ size = "regular" }: { size?: "small" | "regular" }) {
       className={size === "small" ? "w-3 h-3" : ""}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M5.1087 3.625C3.66795 3.625 2.5 4.79295 2.5 6.2337C2.5 7.58604 3.52903 8.69805 4.8468 8.82941V14.7119C4.8468 16.5128 6.30674 17.9727 8.10767 17.9727H11.7598C13.5607 17.9727 15.0206 16.5128 15.0206 14.7119V8.83924C16.4013 8.77184 17.5 7.63108 17.5 6.2337C17.5 4.79295 16.332 3.625 14.8913 3.625H5.1087Z"
         fill="url(#paint0_linear_6729_165920)"
       />
@@ -23,8 +23,8 @@ export function BreadSVG({ size = "regular" }: { size?: "small" | "regular" }) {
           y2="5.8642"
           gradientUnits="userSpaceOnUse"
         >
-          <stop stop-color="#D04EC5" />
-          <stop offset="1" stop-color="#ED7BC7" />
+          <stop stopColor="#D04EC5" />
+          <stop offset="1" stopColor="#ED7BC7" />
         </linearGradient>
       </defs>
     </svg>
