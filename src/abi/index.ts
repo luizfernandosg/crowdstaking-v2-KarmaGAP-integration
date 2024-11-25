80b0979fee68b8a7581a7697ebbdd6a6e51803ea
subject026 <subject026@protonmail.com>
2024-11-25 09:30:44 +0000

    tidy up abis

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -1,26 +1,13 @@
-import SDAI_ADAPTOR from "./SDAIAdaptor.json";
-
+import { sdaiAdaptorAbi } from "./SDAIAdaptor";
 import { breadAbi } from "./Bread";
 import { distributorAbi } from "./Distributor";
 import { butteredBreadAbi } from "./ButteredBread";
 import { multicall3Abi } from "./Multicall3";
 import { erc20ABI } from "wagmi";
 
-let BREAD = breadAbi;
-let DISTRIBUTOR = distributorAbi;
-let BUTTERED_BREAD = butteredBreadAbi;
-
-// if (process.env.NODE_ENV === "development") {
-//   BREAD = require("../../contracts/out/Bread.sol/Bread.json").abi ;
-//   DISTRIBUTOR =
-//     require("../../contracts/out/YieldDistributor.sol/YieldDistributor.json").abi;
-//   BUTTERED_BREAD =
-//     require("../../contracts/out/ButteredBread.sol/ButteredBread.json").abi;
-// }
-
-export const BREAD_ABI = BREAD;
+export const BREAD_ABI = breadAbi;
 export const ERC20_ABI = erc20ABI;
 export const MULTICALL3_ABI = multicall3Abi;
-export const DISTRIBUTOR_ABI = DISTRIBUTOR;
-export const SDAI_ADAPTOR_ABI = SDAI_ADAPTOR;
-export const BUTTERED_BREAD_ABI = BUTTERED_BREAD;
+export const DISTRIBUTOR_ABI = distributorAbi;
+export const SDAI_ADAPTOR_ABI = sdaiAdaptorAbi;
+export const BUTTERED_BREAD_ABI = butteredBreadAbi;
