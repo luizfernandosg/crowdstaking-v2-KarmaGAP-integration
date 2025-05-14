f05684cc84e84d88ed4e8a1780657d7315b38ba9
charlielevine <charliechamp616@gmail.com>
2025-05-14 18:45:26 -0400

    temp fix: remove curve.fi URL

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,4 @@ export const BREAD_ADDRESS = "0xa555d5344f6FB6c65da19e403Cb4c1eC4a1a5Ee3";
 export const BUTTER_ADDRESS = "0xf3d8f3de71657d342db60dd714c8a2ae37eac6b4";
 export const BUTTERED_BREAD_ADDRESS =
   "0x680B581605DC0A6902735a80dE35Cb0Ef6E90865";
-export const CURVE_SWAP_URL =
-  "https://curve.fi/dex/xdai/pools/factory-stable-ng-15/swap/";
+export const CURVE_SWAP_URL = '' // "https://curve.fi/dex/xdai/pools/factory-stable-ng-15/swap/";
